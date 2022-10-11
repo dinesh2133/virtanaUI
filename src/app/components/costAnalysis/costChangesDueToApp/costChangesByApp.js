3ff161d318819e9782c17956107174f40ddb26b6
dinesh2133 <dinesh.misale@nityaobject.com>
2022-10-11 15:54:19 +0530

    cost chainging modal

--- src/app/components/costAnalysis/costChangesDueToApp/costChangesByApp.js
+++ src/app/components/costAnalysis/costChangesDueToApp/costChangesByApp.js
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import './cost.css'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { color } from "@mui/system";
 
 const CostChanges = () =>{
-    return(
+        const obj = [
+            {
+                text: 'Truck Monitoring',
+                price: '28,233',
+                range: 'up',
+                key: 0
+            },
+            {
+                text: 'Cargo Ship Monitoring',
+                price: '43,233',
+                range: 'up',
+                key: 1
+            },
+            {
+                text: 'Logistics Operation',
+                price: '33,233',
+                range: 'down',
+                key: 2
+            },
+            {
+                text: 'Warehouse Backup',
+                price: '15,233',
+                range: 'down',
+                key: 3
+            },
+            {
+                text: 'HR Benefits Portal',
+                price: '10,233',
+                range: 'down',
+                key: 4
+            },
+            {
+                text: 'Truck Monitoring',
+                price: '28,233',
+                range: 'up',
+                key: 0
+            },
+            {
+                text: 'HR Benefits Portal',
+                price: '10,233',
+                range: 'down',
+                key: 4
+            },
+            {
+                text: 'Truck Monitoring',
+                price: '28,233',
+                range: 'up',
+                key: 0
+            }
+        ]
+        const [tableData, setTableData] = useState(obj);
+        return(
         <div className="cost-changes">
-            <h3>Top 10 Cost Changes By Application - December</h3>
+            <h6 style={{paddingTop :'10px'}}>Top 10 Cost Changes By Application - December</h6>
             <button className="float-button">view all</button>
+
+            <section className="table-section">
+                <table className="cost-changing-table">
+                    <tbody>
+                        {
+                            tableData.map((row)=>(
+                                <tr className="table-row">
+                                    <td>{row.text.length > 35 ? row.text.slice(0,35) + "....." : row.text}</td>
+                                    <td>{row.price}</td>
+                                    <td>{row.range === 'up' ? <ArrowUpwardIcon  style={{color: 'red'}} fontSize="small" /> : <ArrowDownwardIcon style={{color:'green'}} fontSize="small" />}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </section>
         </div>
     )
 }
--- src/app/pages/costAnalysis.js
+++ src/app/pages/costAnalysis.js
@@ -7,13 +7,20 @@ import './constAnalysis.css'
 const CostAnalysis = () =>{
    
     return (
-        <div id="costAnalysisDiv" >
-            <MonthlyCosts />
-            <CostTrendBar />
-            {/* <CostChanges /> */}
-            {/* <div style={{width : '100%', backgroundColor: 'grey'}}>
-                <h1>testing</h1>
-            </div> */}
+        <div id="costAnalysisDiv">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-sm-3" style={{padding:'15px 0 0 10px'}}>
+                    <MonthlyCosts />
+                </div>
+                <div className="col-sm-5" style={{padding: '0', paddingLeft: '5px'}}>
+                    <CostTrendBar />
+                </div>
+                <div className="col-sm-4" style={{padding: '15px 10px 0 5px'}}>
+                    <CostChanges />
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
