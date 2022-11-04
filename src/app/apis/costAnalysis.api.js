41ff44eaba9fa5d7c86d4e0ca77c62f1fa8f564f
dinesh2133 <dinesh.misale@nityaobject.com>
2022-11-04 14:41:20 +0530

    fixed re-render issue

--- src/app/apis/costAnalysis.api.js
+++ src/app/apis/costAnalysis.api.js
@@ -1,33 +1,44 @@
 import { get,} from '../helpers/utils/apiMethods';
+import {request, gql} from "graphql-request"
 
-export const getCostTrendData = async () => {
-    const response = await get('http://localhost:3001/costTrendData');
-    // let temp = [9, 7, 8];
-    // localStorage.setItem('costTrendData', JSON.stringify(response));
-    return response;
-}
 
-export const getMonthlyCost = async () => {
-    const response = await get('http://localhost:3001/montlyCost');
-    // setTimeout(response, 3000);
-    return response;
-
-}
-
-export const getTopTenCostChanges = async () => {
-    const response = await get("http://localhost:3001/topTenCostChanges");
-    return response;
+export const GetData = async (query) =>{
+    let temp;
+    await request("http://localhost:5000/graphql", query).then((data)=>{    
+        temp = data;
+    }) 
+    if(temp != undefined){
+        console.log("not undefined");
+        return temp;
+    }
 }
 
-export const getinsights = async () => {
-    const response = await get("http://localhost:3001/insights");
-    return response;
-}
-export const getoptimization = async () => {
-    const response = await get("http://localhost:3001/optimization");
-    return response;
-}
-export const getsavingopportunities = async () => {
-    const response = await get("http://localhost:3001/savingopportunities");
-    return response;
-}
+// export const getCostTrendData = async () => {
+    
+//     return response;
+// }
+
+// export const getMonthlyCost = async () => {
+//     const response = await get('http://localhost:3001/montlyCost');
+//     // setTimeout(response, 3000);
+//     return response;
+
+// }
+
+// export const getTopTenCostChanges = async () => {
+//     const response = await get("http://localhost:3001/topTenCostChanges");
+//     return response;
+// }
+
+// export const getinsights = async () => {
+//     const response = await get("http://localhost:3001/insights");
+//     return response;
+// }
+// export const getoptimization = async () => {
+//     const response = await get("http://localhost:3001/optimization");
+//     return response;
+// }
+// export const getsavingopportunities = async () => {
+//     const response = await get("http://localhost:3001/savingopportunities");
+//     return response;
+// }
