ca23fb9645a89d3c75fd274d01017c47b96bc492
adesh-27 <thedark5272@gmail.com>
2022-11-05 10:45:08 +0530

    adding progresscount components

---
+++ src/app/components/costAnalysis/ProgressCount./ProgressCount.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProgressCount from './ProgressCount';
+import "./ProgressCount.css"
+
+export default {
+    title: 'ProgressCount',
+    component: ProgressCount
+}
+
+export const ProgressCount = () => (<ProgressCount style="customWidth"/>)
